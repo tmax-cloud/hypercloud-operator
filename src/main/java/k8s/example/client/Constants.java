e7e7d5fe2e70472316811817dad83e9c14408fae
taegeon_woo <taegeon_woo@tmax.co.kr>
2020-04-14 00:01:53 +0900

    [mod] NameSpaceHandler.java 분기처리

--- src/main/java/k8s/example/client/Constants.java
+++ src/main/java/k8s/example/client/Constants.java
@@ -2,7 +2,8 @@ package k8s.example.client;
 
 public class Constants {
 	public static final String ISSUER = "Tmax-ProAuth-WebHook";
-	public static final String ACCESS_TOKEN_SECRET_KEY = "Access-Token-Secret-Key";
+//	public static final String ACCESS_TOKEN_SECRET_KEY = "Access-Token-Secret-Key";
+	public static final String ACCESS_TOKEN_SECRET_KEY = "ProAuth_Secret";
 	public static final String REFRESH_TOKEN_SECRET_KEY = "Refresh-Token-Secret-Key";
 
 	public static final String K8S_PREFIX = "hpcd-";
