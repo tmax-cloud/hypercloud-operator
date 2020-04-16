485735dabdd84edf606077c001127225b5ed6860
seonho_choi <seonho_choi@tmax.co.kr>
2020-04-16 12:29:25 +0900

    [mod] serviceinstance 에서 plan 조회 시 namespaced 가 아닌 cluster scope 로 조회하도록 수정

--- src/main/java/k8s/example/client/k8s/K8sApiCaller.java
+++ src/main/java/k8s/example/client/k8s/K8sApiCaller.java
@@ -3680,7 +3680,7 @@ public class K8sApiCaller {
 				template.setParameters(parameters);
 			} else {
 				String planName = inDO.getPlan_id();
-				Object planResponse = customObjectApi.getNamespacedCustomObject("servicecatalog.k8s.io", "v1beta1", inDO.getContext().getNamespace(), "serviceplans", planName);
+				Object planResponse = customObjectApi.getClusterCustomObject("servicecatalog.k8s.io", "v1beta1", "serviceplans", planName);
 				GetPlanDO plan = mapper.readValue(gson.toJson(planResponse), GetPlanDO.class);
 				if(plan.getSpec().getInstanceCreateParameterSchema() != null) {
 					for(String key : plan.getSpec().getInstanceCreateParameterSchema().keySet()) {
