b870053d8a6b9e231837fb39435e61545226af1c
seonho_choi <seonho_choi@tmax.co.kr>
2020-04-21 20:34:58 +0900

    [timer]

--- src/main/java/k8s/example/client/metering/Test.java
+++ src/main/java/k8s/example/client/metering/Test.java
@@ -1,13 +1,33 @@
 package k8s.example.client.metering;
 
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.joda.time.DateTime;
 
 public class Test {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("asdfasdfasdfasdfasdf");
-		DateTime currentTime = new DateTime();
-		System.out.println( "timezone	 : " + currentTime.getZone());
-
+		Date timeToRun = new Date(System.currentTimeMillis() + 5000);
+		   Timer timer = new Timer("061d9446-39f2-4a8d-a091-038d0ff32fc0#catalog");
+		    
+		   timer.schedule(new TimerTask() {
+		           public void run() {
+		               System.out.println("wake!!!!!!!!!!!!!");
+		               System.out.println(Thread.currentThread().getName());
+		           }
+		       }, timeToRun);
+  
+		   
+		   timer.schedule(new TimerTask() {
+	           public void run() {
+	               System.out.println("wake!!!!!!!!!!!!!");
+	               System.out.println(Thread.currentThread().getName());
+	           }
+	       }, timeToRun);
+		   
+		   
+		   
 	}
 }
