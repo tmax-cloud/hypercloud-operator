a0117a9897cc026e878c499e29bf2e1b01175a3c
sukhee_yun <sukhee_yun@tmax.co.kr>
2020-05-22 15:12:21 +0900

    [crd] Registry service 필드 내용 수정

--- src/main/java/k8s/example/client/models/RegistryService.java
+++ src/main/java/k8s/example/client/models/RegistryService.java
@@ -1,6 +1,34 @@
 package k8s.example.client.models;
 
 public class RegistryService {
+	public class Ingress {
+		private int port = 443;
+		private String domainName = null;
+		public int getPort() {
+			return port;
+		}
+		public void setPort(int port) {
+			this.port = port;
+		}
+		public String getDomainName() {
+			return domainName;
+		}
+		public void setDomainName(String domainName) {
+			this.domainName = domainName;
+		}
+	}
+	
+	public class LoadBalancer{
+		private int port = 443;
+
+		public int getPort() {
+			return port;
+		}
+		public void setPort(int port) {
+			this.port = port;
+		}
+	}
+	
 	public static final String SVC_TYPE_CLUSTER_IP = "ClusterIP";
 	public static final String SVC_TYPE_NODE_PORT = "NodePort";
 	public static final String SVC_TYPE_LOAD_BALANCER = "LoadBalancer";
@@ -8,43 +36,22 @@ public class RegistryService {
 	public static final int REGISTRY_TARGET_PORT = 443;
 	public static final String REGISTRY_PORT_NAME = "tls";
 	public static final String REGISTRY_PORT_PROTOCOL = "TCP";
-	private int port = 0;
-	private String ingressDomain = null;
-	private String type = null;
 	
-	public int getPort() {
-		return port;
+	private Ingress ingress = null;
+	private LoadBalancer loadBalancer = null;
+	
+	public Ingress getIngress() {
+		return ingress;
 	}
-	public void setPort(int port) {
-		this.port = port;
+	public void setIngress(Ingress ingress) {
+		this.ingress = ingress;
 	}
-	public String getIngressDomain() {
-		return ingressDomain;
+	public LoadBalancer getLoadBalancer() {
+		return loadBalancer;
 	}
-	public void setIngressDomain(String ingressDomain) {
-		this.ingressDomain = ingressDomain;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
+	public void setLoadBalancer(LoadBalancer loadBalancer) {
+		this.loadBalancer = loadBalancer;
 	}
 	
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("class RegistryService {\n");
-		sb.append("    port: ").append(toIndentedString(String.valueOf(port))).append("\n");
-		sb.append("    ingressDomain: ").append(toIndentedString(ingressDomain)).append("\n");
-		sb.append("    type: ").append(toIndentedString(type)).append("\n");
-		sb.append("}");
-		return sb.toString();
-	}
 	
-	private String toIndentedString(java.lang.Object o) {
-		if (o == null) {
-			return "null";
-		}
-		return o.toString().replace("\n", "\n    ");
-	}
 }
