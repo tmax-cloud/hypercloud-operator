50e742dea5742370b145c99a9cc699df07eb1462
taegeon_woo <taegeon_woo@tmax.co.kr>
2020-08-18 15:39:10 +0900

    [feat] scripts 추가

---
+++ scripts/userMigrationFromK8s.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+apiserver=$1
+echo apiserver : $apiserver
+
+# get Admin Token
+token=$(curl -X POST 'http://'$apiserver':8080/auth/realms/master/protocol/openid-connect/token' \
+ -H "Content-Type: application/x-www-form-urlencoded" \
+ -d "username=admin" \
+ -d 'password=admin' \
+ -d 'grant_type=password' \
+ -d 'client_id=admin-cli' | jq -r '.access_token')
+
+
+echo accessToken : $token
+
+# User Registration
+userIdList=`kubectl get user -o=jsonpath='{.items[*].metadata.name}'`
+
+for userId in $userIdList
+
+do
+	# Get User Email
+	email=`kubectl get user $userId -o=jsonpath='{.userInfo.email}'`
+	dateOfBirth=`kubectl get user $userId -o=jsonpath='{.userInfo.dateOfBirth}' | tr -d ' '`
+	phone=`kubectl get user $userId -o=jsonpath='{.userInfo.phone}' | tr -d ' '`
+	department=`kubectl get user $userId -o=jsonpath='{.userInfo.department}' | tr -d ' '`
+	position=`kubectl get user $userId -o=jsonpath='{.userInfo.position}' | tr -d ' '`
+	description=`kubectl get user $userId -o=jsonpath='{.userInfo.description}' | tr -d ' '`
+
+	echo userId : $userId
+	echo email : $email
+	echo dateOfBirth : $dateOfBirth
+	echo phone : $phone
+	echo department : $department
+	echo description : $description
+	
+	curl -g -i -X POST \
+   -H "Content-Type:application/json" \
+   -H "Authorization:Bearer $token" \
+   -d \
+		'{
+		  "enabled": true,
+		  "attributes": {
+		  	"dateOfBirth": "'$dateOfBirth'",
+		  	"phone": "'$phone'",
+		  	"department": "'$department'",
+		  	"description": "'$description'"
+		  },
+		  "username": "'$userId'",
+		  "emailVerified": "",
+		  "email": "'$email'"
+		}' \
+		 'http://'$apiserver':8080/auth/admin/realms/tmax/users' 
+	echo
+	echo user registration of $userId Complete!!
+	echo ---------------------------------------------------
+	echo
+done
